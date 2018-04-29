fb782d150b266b787b53185ba744118916a8abf1
oatakan <oatakan@gmail.com>
2018-04-29 00:36:10 -0500

    change cfme version from 58 to 591

--- roles/provision-ansible-tower/templates/cloudforms.py
+++ roles/provision-ansible-tower/templates/cloudforms.py
@@ -261,7 +261,8 @@ class CloudFormsInventory(object):
 
         while not last_page:
             offset = page * limit
-            ret = self._get_json("%s/api/vms?offset=%s&limit=%s&expand=resources,tags,hosts,&attributes=ipaddresses" % (self.cloudforms_url, offset, limit))
+            #ret = self._get_json("%s/api/vms?offset=%s&limit=%s&expand=resources,tags,hosts,&attributes=ipaddresses" % (self.cloudforms_url, offset, limit))
+            ret = self._get_json("%s/api/vms?filter[]=vendor=vmware&filter[]=archived=no&offset=%s&limit=%s&expand=resources,tags,hosts,&attributes=ipaddresses" % (self.cloudforms_url, offset, limit))
             results += ret['resources']
             if ret['subcount'] < limit:
                 last_page = True
